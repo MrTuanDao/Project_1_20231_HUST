d56ee72a87de347ac171d7c3e112c7e0a6bac220
MrTuanDao <103438257+MrTuanDao@users.noreply.github.com>
2023-09-27 19:06:44 +0700

    Create extract_yyyy_mm_dd.cpp

---
+++ Tuan_1/extract_yyyy_mm_dd.cpp
@@ -0,0 +1,27 @@
+//CPP 
+#include <bits/stdc++.h> 
+using namespace std;
+
+bool check(string date){
+    //format YYYY-MM-DD
+    //check length
+    if(date.size() > 10) return false;
+    //check "-"
+    if(date[4] != '-' || date[7] != '-') return false;
+    //check MM
+    int month = (date[5] - '0')*10 + (date[6] - '0');
+    if(month > 12 ) return false;
+    //check DD
+    int day = (date[8] - '0')*10 + (date[9] - '0');
+    if(day > 30 ) return false;
+  
+    cout<< date.substr(0, 4) << ' ' << month << ' ' <<day;
+    return true;
+}
+
+int main() 
+{ 
+    string date; getline(cin, date);
+    if(!check(date)) cout<< "INCORRECT";
+    return 0;
+}
