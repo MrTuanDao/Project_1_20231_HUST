d1e6e2c4cc820105514072c49fa6c7b3e508faf5
MrTuanDao <103438257+MrTuanDao@users.noreply.github.com>
2023-09-27 20:27:22 +0700

    Create count_word.cpp

---
+++ Tuan_1/count_word.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int Q = 0;
+    while(cin){
+        string str1; cin>>str1;
+        if(str1 == "") Q --;
+        Q++;
+    }
+    cout<<Q;
+    return 0;
+}
