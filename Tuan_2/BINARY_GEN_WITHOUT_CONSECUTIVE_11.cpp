82f8fffd0cca3e9465fd00a3e5210ecd8c0ea704
MrTuanDao <103438257+MrTuanDao@users.noreply.github.com>
2023-10-02 20:55:25 +0700

    Create BINARY_GEN_WITHOUT_CONSECUTIVE_11.cpp

---
+++ Tuan_2/BINARY_GEN_WITHOUT_CONSECUTIVE_11.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 30
+
+int a[MAXN];
+int n;
+
+void GhiNhan(){
+    for(int i = 1 ; i <= n; i++){
+        cout<<a[i];
+    }
+    cout<<endl;
+}
+
+void backTracking(int k){
+    for(int i = 0; i <= 1; i++){
+        if(i == 1 && a[k-1] == 1) continue;
+        a[k] = i;
+        if(k==n) GhiNhan();
+        else if(k < n) backTracking(k+1);
+    }
+}
+
+int main(){
+    cin>>n;
+    backTracking(1);
+    return 0;
+}
